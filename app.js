663efc24021253374d6e8f35aa8bd1ee37a1a3c2
Sabrina Koumoin <sabrina.koumoin@gmail.com>
2018-01-18 03:07:42 -0500

    first commit

--- app.js
+++ app.js
@@ -6,38 +6,36 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require("passport")
 var flash = require("connect-flash")
-var session = require("express-session")
+var session = require("express-session")  
 var mongoose = require("mongoose")
 
-
-
 var MongoClient = require('mongodb').MongoClient;
-var uri  = "mongodb://sabrinakoumoin:abidjan54@cluster0-shard-00-00-z9s7w.mongodb.net:27017,cluster0-shard-00-01-z9s7w.mongodb.net:27017,cluster0-shard-00-02-z9s7w.mongodb.net:27017/authSource=Admin&ssl=true&replicaSet=Cluster0-shard-0"
+var uri = "mongodb://sabrinakoumoin:abidjan54@cluster0-shard-00-00-z9s7w.mongodb.net:27017,cluster0-shard-00-01-z9s7w.mongodb.net:27017,cluster0-shard-00-02-z9s7w.mongodb.net:27017/authSource=Admin&ssl=true&replicaSet=Cluster0-shard-0"
 var uri2 = "mongodb://sabrinakoumoin:abidjan54@ds051960.mlab.com:51960/bintoudb"
 var uri3 = "mongodb://127.0.0.1/27017/newproject"
 var uri4 = "mongodb://mgis.students1:bingerville225@ds151279.mlab.com:51279/mymgis1"
-//4 is real ok
-//127.0.0.1/27017:3000/mymgis
-//for mongo shell: mongo ds155405-a0.mlab.com:55405/afribelle -u sabrinakoumoin -p abidjan54
-// mongo ds151279.mlab.com:51279/mymgis1 -u <dbuser> -p <dbpassword>
+var urii = "mongodb://sabrinakoumoin:abidjan54@ds261247.mlab.com:61247/mpa2"
+    //4 is real ok  
+    //127.0.0.1/27017:3000/mymgis
+    //for mongo shell: mongo ds155405-a0.mlab.com:55405/afribelle -u sabrinakoumoin -p abidjan54
+    // mongo ds151279.mlab.com:51279/mymgis1 -u <dbuser> -p <dbpassword>
 
-mongoose.connect(uri4, {
+mongoose.connect(urii, {
     replset: {
-      connectWithNoPrimary:true,
-      auto_reconnect: true,
-      ssl: true,
-      sslValidate: false
-   
+        connectWithNoPrimary: true,
+        auto_reconnect: true,
+        ssl: true,
+        sslValidate: false
+
     }
-  }, function(err) {
+}, function(err) {
     if (err) {
-      console.log(err);
-      process.exit();
+        console.log(err);
+        process.exit();
+    } else {
+        console.log("Everything is ok :D");
     }
-    else {
-      console.log("Everything is ok :D");
-    }
-  });
+});
 
 
 
@@ -61,12 +59,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Site icon
-app.use(favicon(path.join(__dirname, 'public', "images","icons",'siteIcon.ico')));
+app.use(favicon(path.join(__dirname, 'public', "images", "icons", 'siteIcon.ico')));
 
 
 // required for passport
-app.use(session({ secret: 'sabrinayes', resave: true,
-    saveUninitialized: true })); // session secret
+app.use(session({
+    secret: 'sabrinayes',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
@@ -74,15 +75,15 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 //Routing
-var users = require('./routes/users')(app,passport);
+var users = require('./routes/users')(app, passport);
 var routes = require('./routes/index')(app)
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -90,23 +91,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
